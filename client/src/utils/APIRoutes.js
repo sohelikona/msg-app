38f14902be1ab70716bd20938a9eb7886344f5bf
sohelikona <kritiakter0@gmail.com>
2023-12-09 16:47:19 +0600

    Update APIRoutes.js

--- client/src/utils/APIRoutes.js
+++ client/src/utils/APIRoutes.js
@@ -1,4 +1,5 @@
-export const host = "http://localhost:5555";
+export const host = "https://kona-a8bn.onrender.com";
+
 export const registerRoute = `${host}/api/auth/register`;
 export const loginRoute = `${host}/api/auth/login`;
 export const setAvatarRoute = `${host}/api/auth/setAvatar`;
